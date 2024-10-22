12009f5f44690aef9bf2ec14e4151f67ee38e1fe
Stylianos <stylianos.kz@hotmail.com>
2024-10-22 14:38:46 +0100

    Added depedencies for hibernate and added config

--- Backend/src/main/java/com/example/Backend/Backend.java
+++
@@ -1,6 +0,0 @@
-@SpringBootApplication
-public class MyBackendAppApplication {
-    public static void main(String[] args) {
-        SpringApplication.run(MyBackendAppApplication.class, args);
-    }
-}
