0b4ff89af0ba2c40128f2f886ef9f1aca8e65444
William <wostler@live.co.uk>
2024-10-23 13:57:05 +0100

    Add reservation page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import './App.css';
 import BookList from './components/BookList';
+import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+
+// Placeholder component for 'Your Reservations' page
+const YourReservations = () => (
+  <div>
+    <h2>Your Reservations</h2>
+    {/* Add your reservations content here */}
+  </div>
+);
 
 function App() {
-    return (
-        <div className="App">
-            <BookList />
-        </div>
-    );
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <Link to="/">Home</Link>
+          <Link to="/reservations">Your Reservations</Link>
+        </nav>
+        <Routes>
+          <Route path="/" element={<BookList />} />
+          <Route path="/reservations" element={<YourReservations />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
