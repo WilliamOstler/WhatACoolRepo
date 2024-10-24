ad5b1ac04b6df70e6b7ef9a4aa52732571eaaec1
William <wostler@live.co.uk>
2024-10-24 10:54:14 +0100

    added alert

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -54,6 +54,7 @@ function App() {
                 <div className="notifications-dropdown">
                   {notifications.map((notification, index) => (
                     <div key={index} className="notification-item">
+                      <img src="alert-siren.gif" alt="Notification GIF" className="notification-gif" />
                       <p>{notification}</p>
                       <button onClick={() => handleCloseNotification(index)}>X</button>
                     </div>
