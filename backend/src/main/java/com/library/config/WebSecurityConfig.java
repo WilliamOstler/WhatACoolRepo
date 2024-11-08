8ff3fe41603f85a7159413f3d759d381d375d994
Stylianos Kyzis <stylianos.kz@hotmail.com>
2024-11-08 10:30:13 +0000

    Tryying to fix Cors through config

--- backend/src/main/java/com/library/config/WebSecurityConfig.java
+++ backend/src/main/java/com/library/config/WebSecurityConfig.java
@@ -6,6 +6,9 @@ import org.springframework.context.annotation.Primary;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @Primary
 @Configuration
@@ -13,17 +16,28 @@ public class WebSecurityConfig {
 
     @Bean(name = "customSecurityFilterChain")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http
-                .authorizeRequests()
-                .requestMatchers(HttpMethod.GET, "/api/**").permitAll()  // Allow all GET requests to /api/*
-                .requestMatchers(HttpMethod.POST, "/api/**").permitAll() // Allow all POST requests to /api/*
-                .requestMatchers(HttpMethod.PUT, "/api/**").permitAll()  // Allow all PUT requests to /api/*
-                .requestMatchers(HttpMethod.DELETE, "/api/**").permitAll() // Allow all DELETE requests to /api/*
-                .anyRequest().permitAll() // Allow any other requests
-                .and()
-                .csrf().disable()  // Disable CSRF protection for APIs
-                .httpBasic();  // Use basic authentication (if necessary)
+        http.cors().and()
+                .authorizeHttpRequests(authz -> authz
+                        .requestMatchers("/**").permitAll()  // Allow all requests
+                        .anyRequest().authenticated());  // All other requests need authentication
 
         return http.build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.addAllowedOrigin("http://localhost:3000");  // Frontend URL for development
+        config.addAllowedOrigin("http://35.210.27.73");  // Your server's IP address
+        config.addAllowedHeader("*");
+        config.addAllowedMethod(HttpMethod.GET);
+        config.addAllowedMethod(HttpMethod.POST);
+        config.addAllowedMethod(HttpMethod.PUT);
+        config.addAllowedMethod(HttpMethod.DELETE);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 }
