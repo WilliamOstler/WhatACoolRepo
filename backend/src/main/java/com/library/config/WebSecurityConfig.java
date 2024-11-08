633e53da294c4de1eacfaaedd4628d08ac175f6d
Stylianos Kyzis <stylianos.kz@hotmail.com>
2024-11-08 13:36:21 +0000

    added som things

--- backend/src/main/java/com/library/config/WebSecurityConfig.java
+++ backend/src/main/java/com/library/config/WebSecurityConfig.java
@@ -1,6 +1,7 @@
+package com.library.config;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Primary;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,7 +11,6 @@ import org.springframework.web.filter.CorsFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Primary
 @Configuration
 public class WebSecurityConfig {
 
