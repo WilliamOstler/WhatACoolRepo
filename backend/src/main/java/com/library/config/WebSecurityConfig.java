8783d635324711ea472a50b046c54c4a8ae05f1e
Stylianos Kyzis <stylianos.kz@hotmail.com>
2024-11-08 11:10:43 +0000

    fadsfas

--- backend/src/main/java/com/library/config/WebSecurityConfig.java
+++ backend/src/main/java/com/library/config/WebSecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
@@ -16,32 +17,20 @@ public class WebSecurityConfig {
 
     @Bean(name = "customSecurityFilterChain")
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.cors().and()
+        http.cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/**").permitAll()  // Allow all requests
-                        .anyRequest().authenticated());  // All other requests need authentication
+                        .requestMatchers("/**").permitAll())
+                .csrf(csrf -> csrf.disable()); // Disable CSRF if not needed
 
         return http.build();
     }
 
-    // This will be used for custom CORS settings
     @Bean
-    public CorsFilter corsFilter() {
+    public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-
-        config.addAllowedOriginPattern("*");
-
-        // Explicitly list allowed origins
-//        config.addAllowedOrigin("http://localhost:3000");  // Your frontend URL
-//        config.addAllowedOrigin("http://35.210.27.73");    // Your production backend IP
-//        config.addAllowedOrigin("http://35.210.27.73:80");
-//        config.addAllowedOrigin("http://35.210.27.73:3000");
-//        config.addAllowedOrigin("http://35.210.27.73:8080");
-
-        // Or use allowedOriginPatterns for pattern matching (if necessary)
-        // config.addAllowedOriginPattern("http://*.yourdomain.com");
-
+        config.addAllowedOriginPattern("http://localhost:3000");  // Frontend URL
+        config.addAllowedOriginPattern("http://35.210.27.73");    // Backend IP
         config.addAllowedHeader("*");
         config.addAllowedMethod(HttpMethod.GET);
         config.addAllowedMethod(HttpMethod.POST);
@@ -50,7 +39,11 @@ public class WebSecurityConfig {
 
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
+        return source;
+    }
 
+    @Bean
+    public CorsFilter corsFilter() {
+        return new CorsFilter(corsConfigurationSource());
     }
 }
